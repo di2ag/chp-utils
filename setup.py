ebcdb1daf74e21c81b2c6b3655fad28a0ced95b3
veenhouse <luke.veenhuis@gmail.com>
2021-07-27 11:02:21 -0500

    added

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup file for chp-utils"""
+from setuptools import setup
+
+with open('README.md', 'r') as stream:
+    long_description = stream.read()
+
+setup(
+    name = 'chp_utils',
+    version = '0.0.0',
+    author = 'Luke Veenhuis',
+    author_email = 'luke.j.veenhuis@dartmouth.edu',
+    url = 'https://github.com/di2ag/chp-utils',
+    description = 'A set of tools for any chp api',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages =[
+        'meta_kg',
+        'meta_kg_validation',
+        'semantic_operations'
+    ],
+    package_data = {
+        'schemas':['*.json']
+    },
+    include_package_data = True,
+    install_requires=[],
+    zip_safe=False,
+    python_requires='>=3.6'
+)
