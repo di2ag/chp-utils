794d2efc382ca46c25b49b45db21cc4a8a7fbb25
Chase Yakaboski <chase.th@dartmouth.edu>
2021-07-27 22:43:31 -0400

    Utilities for querying node normalizer and ontology KP.

---
+++ chp_utils/mixins/sri_ontology_kp.py
@@ -0,0 +1,83 @@
+from collections import defaultdict
+
+from trapi_model.biolink.constants import *
+
+class SriOntologyKpMixin:
+    def _query(self, ontology_query, **kwargs):
+        """ Returns all ontological descendents that are present in a given ONTOLOGY query. The KP
+        will iteratively go through the query and list all descendents if the curie and biolink type are 
+        supported by the SRI Ontology KP.
+
+        :param ontology_query: The specific ontology query that is created via the _build_ontology_query method.
+        :type ontology_query: dict
+        """
+        _url = self.url + 'query'
+        verbose = kwargs.pop('verbose', True)
+        from_cache, out = self._post(_url, params=ontology_query, verbose=verbose)
+        if verbose and from_cache:
+            print('Result from cache.')
+        return out.json()
+
+    def _build_ontology_query(self, curies, biolink_entity):
+        """ Have to load a special form of TRAPI message, so we'll just build it
+        without the help of the trapi_model.
+
+        :param curies: A list of containing all the curies you want ontological descendents.
+        :type curies: list
+        :param biolink_entity: The Biolink Entitiy that pertains to the curies.
+        :type biolink_entity: trapi_model.biolink.BiolinkEntity
+        
+        :returns: A dictionary of the Ontology KP result.
+        :rtype: dict
+        """
+        # Build weird Ontology KP Query graph.
+        query_graph = {
+                "nodes": {
+                    "n0": {
+                        "ids": curies
+                        },
+                    "n1": {
+                        "categories": [biolink_entity.get_curie()]
+                        },
+                    },
+                "edges": {
+                    "e0": {
+                        "subject": "n1",
+                        "object": "n0",
+                        "predicates": [BIOLINK_SUBCLASS_OF_ENTITY.get_curie()],
+                        }
+                    }
+                }
+        # Wrap in a message and in a query
+        query = {
+                "message": {
+                    "query_graph": query_graph,
+                    }
+                }
+
+        return query
+
+    def _get_ontology_descendents(self, curies, biolink_entity, **kwargs):
+        """ Wrapper function that builds an ontology KP query from a list of curies and associated
+        biolink entity and wraps the Ontology KP query endpoint.
+
+        :param curies: A list of containing all the curies you want ontological descendents.
+        :type curies: list
+        :param biolink_entity: The Biolink Entitiy that pertains to the curies.
+        :type biolink_entity: trapi_model.biolink.BiolinkEntity
+        
+        :returns: A dictionary of the Ontology KP result.
+        :rtype: dict
+        """
+        ontology_query = self._build_ontology_query(curies, biolink_entity)
+        # Pass to query endpoint
+        resp = self._query(ontology_query, **kwargs)
+        return self._parse_ontology_descendents_response(resp)
+
+    def _parse_ontology_descendents_response(self, resp):
+        parse = defaultdict(list)
+        for res in resp["message"]["results"]:
+            curie = res["node_bindings"]["n0"][0]["id"]
+            descendent = res["node_bindings"]["n1"][0]["id"]
+            parse[curie].append(descendent)
+        return dict(parse)
